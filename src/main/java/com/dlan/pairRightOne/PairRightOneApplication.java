3e54e1b3bf131e98307696aff70065877cc91c25
188 <a188@188s-MacBook-Pro.local>
2023-07-07 00:22:10 +0800

    initial commit

---
+++ src/main/java/com/dlan/pairRightOne/PairRightOneApplication.java
@@ -0,0 +1,13 @@
+package com.dlan.pairRightOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairRightOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairRightOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlan/pairRightOne/PairRightOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlan.pairRightOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairRightOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
